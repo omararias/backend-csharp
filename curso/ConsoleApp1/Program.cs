7bed1c97b9c6c861850781e94d6bccb401bc6626
Omar Arias <72229639+omararias@users.noreply.github.com>
2025-01-14 16:56:08 -0600

    first projects upload
    load first 2 projects, which are about functional programming in c#

---
+++ curso/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//programacion funcional
+//omar arias rocha
+
+
+
+
+
+Console.WriteLine(Pura (3,2));
+
+int Pura(int a, int b)
+{
+    return a + b;
+}
+
+int
