717adb45d8ce95de651fb3d5585273447f4715f9
Omar Arias <72229639+omararias@users.noreply.github.com>
2025-01-15 13:25:13 -0600

    add proyecto api
    Creacion de primer api, testeando los metodos get post put delete

---
+++ ProjectAPI/ProjectAPI/Controllers/OperationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Add(decimal a, decimal b)
+        {
+            decimal resultado = a + b;
+            return resultado;
+        }
+
+        [HttpPost]
+        public decimal Resta(decimal a, decimal b)
+        {
+            decimal resultado = a - b;
+            return resultado;
+        }
+        [HttpPut]
+        public decimal Edit(decimal a, decimal b)
+        {
+            decimal resultado = a * b;
+            return resultado;
+        }
+
+        [HttpDelete]
+        public decimal Delete(decimal a, decimal b)
+        {
+            decimal resultado = a / b;
+            return resultado;
+        }
+    }
+}
