f58913d01c3e12d45cba419d48b05e010a20f611
Omar Arias <72229639+omararias@users.noreply.github.com>
2025-01-27 14:27:38 -0600

    add controller
    se agrega controlador que sirve de explicacion de programacion sincrona para posteriormente agregar uno ejemplificando la asincrona

---
+++ ProjectAPI/ProjectAPI/Controllers/SomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet ("sync")]
+        public IActionResult GetSync()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+
+            Thread.Sleep(2000);
+            Console.WriteLine("conexion a la BD realizada");
+
+            Thread.Sleep(2000);
+            Console.WriteLine("Envío de mail realizado");
+
+            Console.WriteLine("Todo listo");
+
+            stopwatch.Stop();
+
+            return Ok(stopwatch.Elapsed);
+
+        }
+    }
+}
