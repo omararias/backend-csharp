bf6bb805ab51bfabef01f7672a6e9e46f7e33a27
Omar Arias <72229639+omararias@users.noreply.github.com>
2025-01-28 10:04:32 -0600

    mod SomeController
    added asyncronous method to this controller so i can compare times of execution between async and sync methods

--- ProjectAPI/ProjectAPI/Controllers/SomeController.cs
+++ ProjectAPI/ProjectAPI/Controllers/SomeController.cs
@@ -15,10 +15,10 @@ namespace ProjectAPI.Controllers
             stopwatch.Start();
 
 
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
             Console.WriteLine("conexion a la BD realizada");
 
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
             Console.WriteLine("Envío de mail realizado");
 
             Console.WriteLine("Todo listo");
@@ -27,6 +27,47 @@ namespace ProjectAPI.Controllers
 
             return Ok(stopwatch.Elapsed);
 
+        }
+
+        [HttpGet("async")]
+        public async Task<IActionResult> GetAsync()
+        {
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var task1 = new Task<int>(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("conexion a la BD realizada");
+
+                return 1;
+
+
+            });
+
+            var task2 = new Task<int>(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Envio de mensaje whatsapp realizado");
+
+                return 2;
+
+
+            });
+
+            task1.Start();
+            task2.Start();
+            Console.WriteLine("impresion asincrona");
+
+            var resultado1=await task1;
+            var resultado2 = await task2;
+
+            Console.WriteLine("final de la ejecucion");
+
+            stopwatch.Stop();
+
+            return Ok(resultado1+" tiempo transcurrido"+stopwatch.Elapsed+"  --"+resultado2);
         }
     }
 }
