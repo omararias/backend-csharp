f8688c16361ee3a729cc980ac2bc900a4e7bd148
Omar Arias <72229639+omararias@users.noreply.github.com>
2025-01-29 12:39:54 -0600

    create beer and brand class
    in order to use to practice the ORM and context between Databases and classes

---
+++ ProjectAPI/ProjectAPI/Models/Beer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectAPI.Models
+{
+    public class Beer
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string BrandId { get; set; }
+
+        [ForeignKey("BrandId")]
+
+        public virtual Brand Brand { get; set; }
+    }
+}
